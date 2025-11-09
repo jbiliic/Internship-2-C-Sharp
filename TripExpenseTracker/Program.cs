cf4c0f5d9482e0e7d973da9e7b5d130af2a131a7
jbiliic <jopab03@gmail.com>
2025-11-09 17:05:00 +0100

    created core app skeleton , userScreen UI , tripScreen UI

---
+++ TripExpenseTracker/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripExpenseTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 'database' creation
+            var userDataBase = new Dictionary<int, string[]>();
+            var tripDataBase = new Dictionary<int, string[]>();
+
+            while (true) {
+                Console.Clear();
+                Console.Write("1 - Korisnici\r\n2 - Putovanja\r\n0 - Izlaz iz aplikacije\r\nUnos:");
+                switch (Console.ReadKey().KeyChar) {
+
+                    case '1':
+                        userScreen(userDataBase, tripDataBase);
+                        break;
+
+                    case '2':
+                        tripScreen(userDataBase, tripDataBase);
+                        break;
+
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("\nNepoznata komanda , pritisnite enter zatim pokušajte ponovo.");
+                        Console.ReadKey();
+                        break;
+
+                }
+
+            }
+        }
+        //
+        static void userScreen(Dictionary<int, string[]> userDataBase, Dictionary<int, string[]> tripDataBase) {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("1 - Unos novog korisnika\r\n2 - Brisanje korisnika\r\n3 - Uređivanje korisnika\r\n4 - Pregled svih korisnika\r\n0 - Povratak na glavni izbornik\r\n");
+            Console.Write("Unos:");
+
+                switch (Console.ReadKey().KeyChar) {
+
+                    case '1':
+
+                        break;
+
+                    case '2':
+
+                        break;
+
+                    case '3':
+
+                        break;
+
+                    case '4':
+
+                        break;
+
+                    case '0':
+                        return;
+                    default:
+                        Console.WriteLine("\nNepoznata komanda , pritisnite enter zatim pokušajte ponovo.");
+                        Console.ReadKey();
+                        break;
+
+
+                }
+            }
+        }
+        static void tripScreen(Dictionary<int, string[]> userDataBase, Dictionary<int, string[]> tripDataBase)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("1 - Unos novog putovanja\r\n2 - Brisanje putovanja\r\n3 - Uređivanje postojećeg putovanja\r\n4 - Pregled svih putovanja\r\n5 - Izvještaji i analize\r\n0 - Povratak na glavni izbornik\r\n");
+                Console.Write("Unos:");
+
+                switch (Console.ReadKey().KeyChar)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    case '5':
+                        break;
+                    case '0':
+                        return;
+                    default:
+                        Console.WriteLine("\nNepoznata komanda , pritisnite enter zatim pokušajte ponovo.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+    }
+}
