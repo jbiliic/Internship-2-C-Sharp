d05d6311c21c721753443a810d0774c2104fcf9a
jbiliic <jopab03@gmail.com>
2025-11-09 18:34:52 +0100

    Added create user function

--- TripExpenseTracker/Program.cs
+++ TripExpenseTracker/Program.cs
@@ -1,23 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace TripExpenseTracker
 {
     internal class Program
     {
+        private static int userIdCounter = 0;
+        private static int tripIdCounter = 0;
+        static int generateUserId() { return userIdCounter++; }
+        static int generateTripId() { return tripIdCounter++; }
         static void Main(string[] args)
         {
             // 'database' creation
             var userDataBase = new Dictionary<int, string[]>();
             var tripDataBase = new Dictionary<int, string[]>();
 
-            while (true) {
+            while (true)
+            {
                 Console.Clear();
                 Console.Write("1 - Korisnici\r\n2 - Putovanja\r\n0 - Izlaz iz aplikacije\r\nUnos:");
-                switch (Console.ReadKey().KeyChar) {
+                switch (Console.ReadKey().KeyChar)
+                {
 
                     case '1':
                         userScreen(userDataBase, tripDataBase);
@@ -39,18 +47,24 @@ namespace TripExpenseTracker
 
             }
         }
-        //
-        static void userScreen(Dictionary<int, string[]> userDataBase, Dictionary<int, string[]> tripDataBase) {
+
+        static void userScreen(Dictionary<int, string[]> userDataBase, Dictionary<int, string[]> tripDataBase)
+        {
             while (true)
             {
                 Console.Clear();
                 Console.Write("1 - Unos novog korisnika\r\n2 - Brisanje korisnika\r\n3 - Uređivanje korisnika\r\n4 - Pregled svih korisnika\r\n0 - Povratak na glavni izbornik\r\n");
-            Console.Write("Unos:");
+                Console.Write("Unos:");
 
-                switch (Console.ReadKey().KeyChar) {
+                switch (Console.ReadKey().KeyChar)
+                {
 
                     case '1':
-
+                        userDataBase.Add(generateUserId(), createNewUser());
+                        foreach (var user in userDataBase) {
+                            Console.WriteLine($" {user.Key} - {user.Value[0]} - {user.Value[1]} - {user.Value[2]}");
+                        }
+                        Console.ReadKey();
                         break;
 
                     case '2':
@@ -104,6 +118,77 @@ namespace TripExpenseTracker
                         break;
                 }
             }
+        }
+        // takes usern input and return an array with user information
+        static string[] createNewUser() {
+
+            string firstName="", lastName="", dateOfBirth="";
+            
+            
+            //get first name
+            while (true) {
+                Console.Clear();
+                Console.Write("\nUnesite ime korisnika: ");
+                firstName = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(firstName) || firstName.Length <2 || firstName.Length >20 || ContainsSpecialCharacters(firstName)) 
+                { Console.Write("\nIme nesmije biti prazno, krace od 2 slova te duze od 20!!! Pritisnite enter te pokusajte ponovno"); Console.ReadKey(); continue; }
+                break;
+            }
+            //get laste name
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("\nUnesite prezime korisnika: ");
+                lastName = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(lastName) || lastName.Length < 2 || lastName.Length > 20 || ContainsSpecialCharacters(lastName))
+                { Console.Write("\nPrezime nesmije biti prazno, krace od 2 slova te duze od 20!!! Pritisnite enter te pokusajte ponovno");Console.ReadKey(); continue; }
+                break;
+            }
+            //get date of birth
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Unesite datum rodenja u formatu dd-mm-yyyy: ");
+                dateOfBirth = Console.ReadLine();
+                try
+                {
+                    DateTime dateOfBirthParased = DateTime.ParseExact(dateOfBirth, "dd-MM-yyyy", null);
+                    if (dateOfBirthParased > DateTime.Now || dateOfBirthParased < DateTime.Now.AddYears(-100))
+                    {
+                        Console.Write("\nUneseni datum je u buducnosti ili je previse u proslosti!!! Pritisnite enter te pokusajte ponovno");
+                        Console.ReadKey();
+                        continue;
+                    }
+                    return new string[] { firstName, lastName, dateOfBirth };
+
+                }
+                catch (Exception)
+                {
+                    Console.Write("\nNeispravan format datuma!!! Pritisnite enter te pokusajte ponovno");
+                    Console.ReadKey();
+                }
+                
+            }
+
+        }
+        /* 
+         * 
+         * 
+         * helper functions
+         * 
+         */
+
+        // checks if string contains special characters
+        static bool ContainsSpecialCharacters(string name)
+        {
+            foreach (char c in name)
+            {
+                if (!char.IsLetter(c))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
